ca09d0a06b0b3e0b42084698a4222dc4c9c4078b
Ali Saleh <alisaleh06@outlook.com.au>
2025-06-18 16:45:05 +1000

    Cool stuff

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://timelessweb.com.au", // ← Replace with your actual domain
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+};
