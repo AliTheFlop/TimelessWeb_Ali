121b27ab8901054a7fc4749546f046c3c685b53a
AS <108449738+AliTheFlop@users.noreply.github.com>
2025-10-12 12:04:09 +1100

    Refactor home page to isolate smooth scrolling client logic

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,34 +1,5 @@
-import dynamic from "next/dynamic";
-import HeroSection from "@/components/sections/Hero";
-import OurWork from "@/components/sections/OurWork";
-import Benefits from "@/components/sections/Benefits";
-import Explanations from "@/components/sections/Explanations";
-import Reviews from "@/components/sections/Reviews";
-import FinalCTA from "@/components/sections/FinalCTA";
-import AnimateOnScroll from "@/components/utils/AnimateOnScroll";
-
-const SmoothScrolling = dynamic(
-    () => import("@/components/utils/SmoothScrolling"),
-    {
-        ssr: false,
-    }
-);
+import HomeClient from "./home-client";
 
 export default function Home() {
-    return (
-        <SmoothScrolling>
-            <HeroSection />
-            <AnimateOnScroll>
-                <Benefits />
-            </AnimateOnScroll>
-            <AnimateOnScroll>
-                <Explanations />
-            </AnimateOnScroll>
-            <AnimateOnScroll>
-                <OurWork />
-            </AnimateOnScroll>
-            <Reviews />
-            <FinalCTA />
-        </SmoothScrolling>
-    );
+    return <HomeClient />;
 }
