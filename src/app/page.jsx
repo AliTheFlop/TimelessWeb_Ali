1f47c2b82808b2a2bad190e52c9fc9fcbe10ed6e
Ali Saleh <alisaleh06@outlook.com.au>
2025-10-12 12:30:05 +1100

    good

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,5 @@
-import HeroSection from "@/components/sections/Hero";
-import OurWork from "@/components/sections/OurWork";
-import Benefits from "@/components/sections/Benefits";
-import Explanations from "@/components/sections/Explanations";
-import Reviews from "@/components/sections/Reviews";
-import FinalCTA from "@/components/sections/FinalCTA";
-import AnimateOnScroll from "@/components/utils/AnimateOnScroll";
-import SmoothScrollingWrapper from "@/components/utils/SmoothScrollingWrapper";
+import HomeClient from "./home-client";
 
 export default function Home() {
-    return (
-        <SmoothScrollingWrapper>
-            <HeroSection />
-            <AnimateOnScroll>
-                <Benefits />
-            </AnimateOnScroll>
-            <AnimateOnScroll>
-                <Explanations />
-            </AnimateOnScroll>
-            <AnimateOnScroll>
-                <OurWork />
-            </AnimateOnScroll>
-            <Reviews />
-            <FinalCTA />
-        </SmoothScrollingWrapper>
-    );
+    return <HomeClient />;
 }
